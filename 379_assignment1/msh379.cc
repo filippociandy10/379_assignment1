5dfb6d3e65fa549e2af0f2a94c8679b74ea29833
filippociandy10 <filippo.ciandy10@gmail.com>
2022-01-29 20:28:35 -0700

    finished until time

--- 379_assignment1/msh379.cc
+++ 379_assignment1/msh379.cc
@@ -1,128 +1,209 @@
+#include <fcntl.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <cmath>
+#include <cstdarg>  //Handling of variable length argument lists
 #include <cstdio>
 #include <cstdlib>
-#include <cassert>
-#include <cmath>
-#include <ctime>
-#include <cassert>
 #include <cstring>
-#include <cstdarg>             //Handling of variable length argument lists
-#include <sys/time.h>
+#include <ctime>
 #include <fstream>
-#include <unistd.h>		
-#include <sys/types.h>
-#include <sys/stat.h>	
-#include <sys/wait.h>
-#include <sys/times.h>
-#include <sys/resource.h>
-#include <fcntl.h>
 #include <iostream>
-#include <chrono>
-#include <string>
 #include <sstream>
-#include <cctype>
+#include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-struct sample {int index; pid_t pid; string command;};
+struct sample {
+    int index;
+    pid_t pid;
+    string cmd;
+};
 
-void pdir(char *ptr,size_t size){
-    if (getcwd(ptr,size) == NULL){
+void pdir(char *ptr, size_t size) {
+    if (getcwd(ptr, size) == NULL) {
         cout << "get cwd failed";
-    }
-    else{
-        cout << ("cwd = %s", ptr);
-        cout << "\n";
+    } else {
+        cout << ptr << endl;
     }
 }
 
-void cdir (vector<string>& arguments){
-    const char* chdir_command;
+void cdir(vector<string> &arguments) {
+    const char *chdir_command;
     chdir_command = (arguments[1]).c_str();
-    if (chdir(chdir_command)<0){
+    if (chdir(chdir_command) < 0) {
         cout << "cdir failed\n";
     }
 }
 
-void run (vector<sample> &tasks,vector<string>& arguments, int counter){
+void run(vector<sample> &tasks, vector<string> &arguments, int counter) {
     pid_t pid;
-    string temp;
+    string cmd;
     int index;
-
     arguments.erase(arguments.begin());
-    vector <char*> argv = vector<char *>(arguments.size());
+    vector<char *> argv = vector<char *>(arguments.size());
     transform(arguments.begin(), arguments.end(), argv.begin(),
-                   [](const string &arg) { return (char *)arg.c_str(); });
+              [](const string &arg) { return (char *)arg.c_str(); });
 
-    if ((pid = fork())<0){
+    argv.push_back(NULL);
+
+    if ((pid = fork()) < 0) {
         cout << "Fork error!";
     }
-    else if (pid == 0){
-        cout << argv[0] << argv.data()[1] <<endl;
-        if (execvp(argv.data()[0],argv.data())<0){
-            cout << "execvp error!" <<endl;
+    if (pid == 0) {
+        if (arguments[0] == "myclock") {
+            execlp("sh", "sh", argv[0], argv[1], NULL);
+        } else {
+            if ((execvp(argv.data()[0], argv.data()) < 0)) {
+                cout << "execvp error!" << endl;
+            }
         }
-        for (int i=0; i<counter;i++){
-            temp = temp.append(arguments[i]);
-            temp = temp.append(" ");
+    } else {
+        for (int i = 0; i < counter - 1; i++) {
+            cmd = cmd.append(arguments[i]);
+            cmd = cmd.append(" ");
         }
         index = tasks.size();
-        sample s = {index,pid,temp};
+        sample s = {index, pid, cmd};
         tasks.push_back(s);
     }
-    cout << "in run " <<endl;
 }
 
-int main(){
+void lstasks(vector<sample> tasks) {
+    for (const auto &elem : tasks) {
+        cout << elem.index << ":    (pid= " << elem.pid << ", cmd= " << elem.cmd
+             << ")" << endl;
+    }
+}
+
+void terminate(vector<sample> &tasks, string taskno) {
+    sample tasks_obj = tasks[stoi(taskno)];
+    pid_t pid_terminate = tasks_obj.pid;
+    kill(pid_terminate, 1);
+    tasks.erase(tasks.begin() + stoi(taskno));
+    cout << tasks_obj.index << ":    (pid= " << tasks_obj.pid
+         << ", cmd= " << tasks_obj.cmd << ")"
+         << " terminated" << endl;
+}
+
+void exit(vector<sample> &tasks) {
+    for (auto sample : tasks) {
+        pid_t pid_terminate = sample.pid;
+        cout << "task " << sample.pid << " terminated." << endl;
+        kill(pid_terminate, 1);
+    }
+}
+
+void stop(vector<sample> &tasks, string taskno) {
+    sample tasks_obj = tasks[stoi(taskno)];
+    pid_t pid_stop = tasks_obj.pid;
+    signal(SIGSTOP, SIG_IGN);
+    kill(pid_stop, SIGSTOP);
+}
+
+void pr_times(clock_t real, struct tms *tmsstart, struct tms *tmsend) {
+    static long clktck = 0;
+    if (clktck == 0) {
+        if ((clktck = sysconf(_SC_CLK_TCK)) < 0) {
+            cout << "sysconf error" << endl;
+        }
+    }
+    cout << (" real: ") << (real / (double)clktck) << endl;
+    cout << (" user: ")
+         << ((tmsend->tms_utime - tmsstart->tms_utime) / (double)clktck)
+         << endl;
+    cout << (" sys: ")
+         << ((tmsend->tms_stime - tmsstart->tms_stime) / (double)clktck)
+         << endl;
+    cout << (" child user:  ")
+         << ((tmsend->tms_cutime - tmsstart->tms_cutime) / (double)clktck)
+         << endl;
+    cout << (" child sys:  ")
+         << ((tmsend->tms_cstime - tmsstart->tms_cstime) / (double)clktck)
+         << endl;
+}
+int main() {
+    // Start time of program
+    struct tms tmsstart, tsmend;
+    clock_t start = times(&tmsstart);
     // Set limit on CPU time
     rlimit rlim_time;
     rlim_time.rlim_cur = 10;
     rlim_time.rlim_max = 2;
-    if (setrlimit(RLIMIT_CPU,&rlim_time) == 0){
+    if (setrlimit(RLIMIT_CPU, &rlim_time) == 0) {
         cout << "Success\n";
     }
-    //Path
-    size_t size= 1024;
-    char *ptr = (char*) malloc(size);
+    // Path
+    size_t size = 1024;
+    char *ptr = (char *)malloc(size);
 
-
-    string input = "" ;
+    string input = "";
     string command = "";
     vector<string> arguments;
-    vector <sample> tasks;
+    vector<sample> tasks;
     string line;
     int counter = 0;
-
-    cout << "msh379 [pid]: ";
-    cin >> input;
-    while (command != "quit"){
-        
+    pid_t pid_process = getpid();
+    cout << "msh379 [" + to_string(pid_process) + "]:";
+    while (command != "quit") {
         // Get all the arguments in cin and puts in a vector
-        getline(cin,line);
+        getline(cin, line);
         std::istringstream stream(line);
-        while(stream.good()){
+        while (stream.good()) {
             string temp;
             stream >> temp;
             arguments.push_back(temp);
+            counter++;
         }
 
-        //pdir command
-        if (arguments[0] == "pdir"){
-            pdir(ptr,size);
+        // pdir command
+        if (arguments[0] == "pdir") {
+            pdir(ptr, size);
         }
-        //cdir command
-        if (arguments[0] == "cdir"){
+        // cdir command
+        if (arguments[0] == "cdir") {
             cdir(arguments);
         }
-        //run command
-        if (arguments[0] == "run"){
-
-            run(tasks,arguments, counter);
+        // run command
+        if (arguments[0] == "run") {
+            // cout << "Counter" << counter <<endl;
+            run(tasks, arguments, counter);
         }
-        //Clear vector and counter after each execution
+        // lstasks command
+        if (arguments[0] == "lstasks") {
+            lstasks(tasks);
+        }
+        // exit command
+        if (arguments[0] == "exit") {
+            exit(tasks);
+            break;
+        }
+        // stop command
+        if (arguments[0] == "stop") {
+            string taskno = arguments[1];
+            stop(tasks, taskno);
+        }
+        // terminate command
+        if (arguments[0] == "terminate") {
+            string taskno = arguments[1];
+            terminate(tasks, taskno);
+        }
+        // Clear vector and counter after each execution
         arguments.clear();
         counter = 0;
-        cout << "msh379 ["+input+"]:";
+        cout << "msh379 [" + to_string(pid_process) + "]:";
     }
+    // Exit program
+    clock_t end = times(&tmsstart);
+    pr_times(end - start, &tmsstart, &tsmend);
 }
-
