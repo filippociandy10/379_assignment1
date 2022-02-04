ef9bafeb08f6a08c3210603dce6636f07291c6a9
filippociandy10 <filippo.ciandy10@gmail.com>
2022-02-04 02:40:49 -0700

    done

--- 379_assignment1/msh379.cc
+++ 379_assignment1/msh379.cc
@@ -1,36 +1,25 @@
-#include <fcntl.h>
+#include <inttypes.h>
 #include <sys/resource.h>
-#include <sys/stat.h>
-#include <sys/time.h>
 #include <sys/times.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include <algorithm>
-#include <cassert>
-#include <cctype>
-#include <chrono>
-#include <cmath>
-#include <cstdarg>  //Handling of variable length argument lists
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <fstream>
 #include <iostream>
-#include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
 
+// Global variables
 struct sample {
     int index;
     pid_t pid;
     string cmd;
 };
+int terminate_counter = 0;
 
+// Prints current path directory
 void pdir(char *ptr, size_t size) {
     if (getcwd(ptr, size) == NULL) {
         std::cout << "get cwd failed";
@@ -39,14 +28,19 @@ void pdir(char *ptr, size_t size) {
     }
 }
 
+// Change current path directory
 void cdir(vector<string> &arguments) {
     const char *chdir_command;
     chdir_command = (arguments[1]).c_str();
     if (chdir(chdir_command) < 0) {
-        std::cout << "cdir failed\n";
+        std::cout << "cdir: failed (pathname = " << chdir_command << ")"
+                  << endl;
+    } else {
+        std::cout << "cdir: done (pathname = " << chdir_command << ")" << endl;
     }
 }
 
+// Run programs by forking
 void run(vector<sample> &tasks, vector<string> &arguments, int counter) {
     pid_t pid;
     string cmd;
@@ -63,9 +57,13 @@ void run(vector<sample> &tasks, vector<string> &arguments, int counter) {
     }
     if (pid == 0) {
         if (arguments[0] == "myclock") {
-            execlp("sh", "sh", argv[0], argv[1], NULL);
+            if (execv("myclock", argv.data()) < 0) {
+                std::cout << "execv error!" << endl;
+            };
         } else if (arguments[0] == "mMyclock") {
-            execlp("sh", "sh", argv[0], NULL);
+            if (execlp("sh", "sh", argv[0], NULL) < 0) {
+                std::cout << "execlp error !" << endl;
+            };
         } else {
             if ((execvp(argv.data()[0], argv.data()) < 0)) {
                 std::cout << "execvp error!" << endl;
@@ -82,6 +80,7 @@ void run(vector<sample> &tasks, vector<string> &arguments, int counter) {
     }
 }
 
+// Prints a list of tasks
 void lstasks(vector<sample> tasks) {
     for (const auto &elem : tasks) {
         std::cout << elem.index << ":    (pid= " << elem.pid
@@ -89,16 +88,7 @@ void lstasks(vector<sample> tasks) {
     }
 }
 
-void terminate(vector<sample> &tasks, string taskno) {
-    sample tasks_obj = tasks[stoi(taskno)];
-    pid_t pid_terminate = tasks_obj.pid;
-    kill(pid_terminate, 1);
-    tasks.erase(tasks.begin() + stoi(taskno));
-    std::cout << tasks_obj.index << ":    (pid= " << tasks_obj.pid
-              << ", cmd= " << tasks_obj.cmd << ")"
-              << " terminated" << endl;
-}
-
+// Exit program by terminating running programs
 void exit(vector<sample> &tasks) {
     for (auto sample : tasks) {
         pid_t pid_terminate = sample.pid;
@@ -107,20 +97,44 @@ void exit(vector<sample> &tasks) {
     }
 }
 
-void stop(vector<sample> &tasks, string taskno) {
-    sample tasks_obj = tasks[stoi(taskno)];
-    pid_t pid_stop = tasks_obj.pid;
-    signal(SIGSTOP, SIG_IGN);
-    kill(pid_stop, SIGSTOP);
+// Helper function for check to print processes
+void print_check(string target_pid, vector<vector<string> > real_records) {
+    vector<string> temp;
+    for (auto x : real_records) {
+        if (x[1] == target_pid) {
+            if (x[3] == "Z") {
+                cout << "target_pid= " + target_pid + " terminated" << endl;
+            } else if (x[3] == "S") {
+                cout << "target_pid= " + target_pid + " is running:" << endl;
+            }
+            std::cout << "USER:      "
+                      << "PID:  "
+                      << "PPID: "
+                      << "S:  "
+                      << "STARTED: "
+                      << "CMD:" << endl;
+            std::cout << x[0] + "  " << x[1] + "  " << x[2] + "  "
+                      << x[3] + "  " << x[4] + "  " << x[5] << endl;
+            for (auto y : real_records) {
+                if (y[2] == target_pid) {
+                    temp.push_back(y[1]);
+                    std::cout << y[0] + "  " << y[1] + "  " << y[2] + "  "
+                              << y[3] + "  " << y[4] + "  " << y[5] << endl;
+                }
+            }
+            for (auto z : temp) {
+                for (auto b : real_records) {
+                    if (z == b[2]) {
+                        std::cout << b[0] + "  " << b[1] + "  " << b[2] + "  "
+                                  << b[3] + "  " << b[4] + "  " << b[5] << endl;
+                    }
+                }
+            }
+        }
+    }
 }
-
-void cont(vector<sample> &tasks, string taskno) {
-    sample tasks_obj = tasks[stoi(taskno)];
-    pid_t pid_continue = tasks_obj.pid;
-    signal(SIGCONT, SIG_IGN);
-    kill(pid_continue, SIGCONT);
-}
-void check(string target_pid) {
+// Check status of processes
+vector<vector<string> > check(string target_pid, bool check_bool) {
     const int MAX_LINE = 1234;
     char line[MAX_LINE];
     FILE *fp;
@@ -142,6 +156,7 @@ void check(string target_pid) {
         fake_records.push_back(temp);
         temp.clear();
     }
+    pclose(fp);
     for (int i = 0; i < (int)fake_records.size(); i++) {
         string line = fake_records[i];
         while (temp_arr.size() < 5) {
@@ -161,24 +176,85 @@ void check(string target_pid) {
         temp_arr.clear();
         index = 0;
     }
+    // If check_bool is false. Continue, stop, terminate functions called it.
+    if (check_bool) {
+        print_check(target_pid, real_records);
+    } else {
+        return real_records;
+    }
+}
+// Terminate process
+void terminate(vector<sample> &tasks, string taskno, int &terminate_counter) {
+    vector<vector<string> > real_records;
+    intmax_t ymax;
+    pid_t y;
+    char *tmp;
+    sample tasks_obj = tasks[stoi(taskno) - terminate_counter];
+    pid_t pid_terminate = tasks_obj.pid;
+
+    real_records = check(to_string(pid_terminate), false);
+    // Get pid where ppid is pid_terminate
     for (auto x : real_records) {
-        if (x[1] == target_pid) {
-            if (x[3] == "Z") {
-                cout << "target_pid= " + target_pid + " terminated" << endl;
-            } else if (x[3] == "S") {
-                cout << "target_pid= " + target_pid + " is running:" << endl;
-            }
-            std::cout << "USER:      "
-                      << "PID:  "
-                      << "PPID: "
-                      << "S:  "
-                      << "STARTED: "
-                      << "CMD:" << endl;
-            std::cout << x[0] + "  " << x[1] + "  " << x[2] + "  "
-                      << x[3] + "  " << x[4] + "  " << x[5] << endl;
+        if (x[2] == to_string(pid_terminate)) {
+            const char *c = x[1].c_str();
+            ymax = strtoimax(c, &tmp, 10);
+            y = (pid_t)ymax;
+            kill(y, 1);
+        }
+    }
+    kill(pid_terminate, 1);
+    tasks.erase(tasks.begin() + (stoi(taskno) - terminate_counter));
+    std::cout << tasks_obj.index << ":    (pid= " << tasks_obj.pid
+              << ", cmd= " << tasks_obj.cmd << ")"
+              << " terminated" << endl;
+    terminate_counter++;
+}
+
+// Stop processes
+void stop(vector<sample> &tasks, string taskno) {
+    vector<vector<string> > real_records;
+    intmax_t ymax;
+    pid_t y;
+    char *tmp;
+    sample tasks_obj = tasks[stoi(taskno)];
+    pid_t pid_stop = tasks_obj.pid;
+    real_records = check(to_string(pid_stop), false);
+
+    kill(pid_stop, SIGSTOP);
+    // Get pid where ppid is pid_stop
+    for (auto x : real_records) {
+        if (x[2] == to_string(pid_stop)) {
+            // Changing string to pid_t
+            const char *c = x[1].c_str();
+            ymax = strtoimax(c, &tmp, 10);
+            y = (pid_t)ymax;
+            kill(y, SIGSTOP);
         }
     }
 }
+
+// Continue task which was stopped
+void cont(vector<sample> &tasks, string taskno) {
+    vector<vector<string> > real_records;
+    intmax_t ymax;
+    pid_t y;
+    char *tmp;
+    sample tasks_obj = tasks[stoi(taskno)];
+    pid_t pid_continue = tasks_obj.pid;
+    real_records = check(to_string(pid_continue), false);
+
+    for (auto x : real_records) {
+        if (x[2] == to_string(pid_continue)) {
+            // Changing string to pid_t
+            const char *c = x[1].c_str();
+            ymax = strtoimax(c, &tmp, 10);
+            y = (pid_t)ymax;
+            kill(y, SIGCONT);
+        }
+    }
+    kill(pid_continue, SIGCONT);
+}
+// Print times on quit/exit
 void pr_times(clock_t real, struct tms *tmsstart, struct tms *tmsend) {
     static long clktck = 0;
     if (clktck == 0) {
@@ -186,19 +262,19 @@ void pr_times(clock_t real, struct tms *tmsstart, struct tms *tmsend) {
             std::cout << "sysconf error" << endl;
         }
     }
-    std::cout << (" real: ") << (real / (double)clktck) << endl;
+    std::cout << (" real: ") << (real / (double)clktck) << " sec." << endl;
     std::cout << (" user: ")
               << ((tmsend->tms_utime - tmsstart->tms_utime) / (double)clktck)
-              << endl;
+              << " sec." << endl;
     std::cout << (" sys: ")
               << ((tmsend->tms_stime - tmsstart->tms_stime) / (double)clktck)
-              << endl;
+              << " sec." << endl;
     std::cout << (" child user:  ")
               << ((tmsend->tms_cutime - tmsstart->tms_cutime) / (double)clktck)
-              << endl;
+              << " sec." << endl;
     std::cout << (" child sys:  ")
               << ((tmsend->tms_cstime - tmsstart->tms_cstime) / (double)clktck)
-              << endl;
+              << " sec." << endl;
 }
 int main() {
     // Start time of program
@@ -206,21 +282,23 @@ int main() {
     clock_t start = times(&tmsstart);
     // Set limit on CPU time
     rlimit rlim_time;
-    rlim_time.rlim_cur = 2;
-    rlim_time.rlim_max = 10;
-    if (setrlimit(RLIMIT_CPU, &rlim_time) == 0) {
-        std::cout << "Set setrlimit successful!" << endl;
-    }
+    rlim_time.rlim_cur = 600;
+    rlim_time.rlim_max = 600;
+    if (setrlimit(RLIMIT_CPU, &rlim_time) == -1) {
+        cout << "SETR LIMIT ERROR!" << endl;
+    };
+
     // Path
     size_t size = 1024;
     char *ptr = (char *)malloc(size);
-
+    bool check_bool = true;
     string input = "";
     string command = "";
     vector<string> arguments;
     vector<sample> tasks;
     string line;
     int counter = 0;
+
     pid_t pid_process = getpid();
     std::cout << "msh379 [" + to_string(pid_process) + "]:";
     while (command != "quit") {
@@ -233,7 +311,10 @@ int main() {
             arguments.push_back(temp);
             counter++;
         }
-
+        // quit command
+        if (arguments[0] == "quit") {
+            break;
+        }
         // pdir command
         if (arguments[0] == "pdir") {
             pdir(ptr, size);
@@ -268,12 +349,14 @@ int main() {
         // terminate command
         if (arguments[0] == "terminate") {
             string taskno = arguments[1];
-            terminate(tasks, taskno);
+            terminate(tasks, taskno, terminate_counter);
         }
         // check command
         if (arguments[0] == "check") {
             string target_pid = arguments[1];
-            check(target_pid);
+            check_bool = true;
+            // If check_bool is true, it means check command
+            check(target_pid, check_bool);
         }
         // Clear vector and counter after each execution
         arguments.clear();
